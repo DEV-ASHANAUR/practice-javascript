45ecdc4acc19e0daaaff5b83beacc4c8f924e029
dev-ashanaur <devashanaur009@gmail.com.com>
2021-05-03 12:50:14 +0600

    wow variable concept remaind

---
+++ variable.js
@@ -0,0 +1,8 @@
+// variable is a container which can store data
+
+var name = 'Md Ashanaur Rahman';
+var age = 22;
+
+console.log(name +' khows javascript');
+console.log('But his Age is '+age);
+console.log('But again '+name+ ' learn javascript');
