78cff7e622b1839fd105ff954d9fcd25510a0231
dev-ashanaur <devashanaur009@gmail.com.com>
2021-05-03 13:12:34 +0600

    dataType file added and khows about defferent  dataType

---
+++ dataType.js
@@ -0,0 +1,10 @@
+//Two Type of dataType in javascript 
+// 1. primitive - Define by Javascript is khown as promitive datatype
+    //* number - 12,4.2,3.14 etc
+    //* string - 'hello world' + "hey What about You"
+    //*boolean - true false undefined null
+
+// 2. object - Define by user is khown as object Type dataType
+    //*array 
+    //*object 
+    //*function
