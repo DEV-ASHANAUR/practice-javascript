b4b8e20aef716bd8f5ef601bb1891b19eb59091d
dev-ashanaur <devashanaur009@gmail.com.com>
2021-05-12 01:23:09 +0600

    learn switch statement

--- condition.js
+++ condition.js
@@ -23,30 +23,65 @@
 
 // if-else condition 
 
-var a = 10;
-var b = 20;
- if(a > b){
-     console.log('a is greater then b');
- }else{
-     console.log('a is less then b');
- }
+// var a = 10;
+// var b = 20;
+//  if(a > b){
+//      console.log('a is greater then b');
+//  }else{
+//      console.log('a is less then b');
+//  }
 
- var n = 50;
- // check odd or even
- if(n % 2 === 0){
-     console.log(n+ ' is a even number');
- }else{
-     console.log(n+ ' is a odd number');
- }
+//  var n = 50;
+//  // check odd or even
+//  if(n % 2 === 0){
+//      console.log(n+ ' is a even number');
+//  }else{
+//      console.log(n+ ' is a odd number');
+//  }
 
  //else-if
 
-if(n === 0){
-    console.log('n is zero');
-}else if(n % 2 === 0){
-    console.log(n+ ' is a even number');
-}else{
-    console.log(n+ ' is a odd number');
+// if(n === 0){
+//     console.log('n is zero');
+// }else if(n % 2 === 0){
+//     console.log(n+ ' is a even number');
+// }else{
+//     console.log(n+ ' is a odd number');
+// }
+
+//switch statement
+
+var date = new Date();
+
+var today = date.getDay();
+// sunday - 0 , monday - 2
+// console.log(today);
+
+switch(today){
+    case 0:
+        console.log('Today is sunday');
+        break;
+    case 1:
+        console.log('Today is monday');
+        break;
+    case 2:
+        console.log('Today is Tuesday');
+        break;
+    case 3:
+        console.log('Today is Wednesday');
+        break;
+    case 4:
+        console.log('Today is Thuesday');
+        break;
+    case 5:
+        console.log('Today is Friday');
+        break;
+    case 6:   
+        console.log('Today is Saturday');
+        break;
+    default:
+        console.log("invalid Number"); 
+        break;                    
 }
 
 
