ee3ac8f746d9421cfbc566367a710f0cf161e327
dev-ashanaur <devashanaur009@gmail.com.com>
2021-05-12 00:01:52 +0600

    operator.js file added

---
+++ operator.js
@@ -0,0 +1,61 @@
+//arithmetic operator
+// + - * / % ++ --
+
+// var a = 11;
+// var b = 5;
+// console.log(a % b);
+
+//console.log(a++); // ans 11
+//console.log(a); // after ans 12
+//console.log(++a); // ans 12
+
+
+//assignment operator
+// a+=b;
+// console.log(a); // ans 16
+
+// a-=b;
+// console.log(a); // ans is 6
+
+// comparison opertator
+
+// var a = 10;
+// var b = 20;
+
+// console.log(a == b); //ans false
+// console.log(a != b) // ans true
+
+// console.log(a > b) //ans false
+// console.log(a < b) //ans true
+
+// console.log(a >= b) //ans false
+// console.log(a <= b) //ans true
+
+// var a = '50';
+// var b = 50;
+// console.log(a == b); //ans true
+// console.log(a === b); //ans false
+// console.log(a !== b) //ans true
+
+// Logical operator 
+
+// && logical and
+// || logical or
+// ! logical not
+
+//Bitwise oparator
+
+// & use for binnery and operation
+// | use for binnery | operation
+// ~ use for binnery not operation
+// ^
+// << left shift
+// >> right shift
+
+// type of operator 
+
+console.log(typeof 78); // ans number
+console.log(typeof 'habijabi'); // ans string
+
+
+
