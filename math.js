41b7085ada0d1514fd06f4ea9b1b38bd8a25f429
dev-ashanaur <devashanaur009@gmail.com.com>
2021-05-12 00:24:44 +0600

    learn  about Math function

---
+++ math.js
@@ -0,0 +1,19 @@
+console.log(Math.E);
+console.log(Math.PI);
+
+var n = 4.589;
+console.log(Math.abs(n));
+console.log(Math.floor(n));
+console.log(Math.ceil(n));
+console.log(Math.round(n));
+console.log(Math.max(400,500,600));
+console.log(Math.min(400,500,600));
+console.log(Math.pow(2,3)); // use for to the power
+console.log(Math.pow(2,4));
+console.log(Math.sqrt(64)); // use for root
+console.log(Math.sqrt(9));
+console.log(Math.random()); // create random number
+
+console.log(Math.round(Math.random() * 50 + 1)); //  give random number into 1 - 50 
+
+console.log(Math.round(Math.random() * 100 + 1)); //  give random number into 1 - 100 
