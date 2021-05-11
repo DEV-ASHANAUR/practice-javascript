e3681f7352722e4f045f88114a80d462a884829c
dev-ashanaur <devashanaur009@gmail.com.com>
2021-05-12 00:58:56 +0600

    learn about if condition

---
+++ condition.js
@@ -0,0 +1,20 @@
+var a = 10;
+var b = 20;
+
+if(a > b){
+    console.log('a is greater then b');
+}
+if(a < b){
+    console.log('a is less then b');
+}
+
+var n = 50;
+
+// check n is even or odd
+if(n % 2 == 0){
+    console.log(n+' is even number');
+}
+
+if(n % 2 == 1){
+    console.log(n+' is Odd number');
+}
