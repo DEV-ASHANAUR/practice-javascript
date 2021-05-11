0314e59c0638a85f69e0f8c54e212349e9a63f4f
dev-ashanaur <devashanaur009@gmail.com.com>
2021-05-12 01:03:47 +0600

    learn if-else condition

--- condition.js
+++ condition.js
@@ -1,20 +1,41 @@
+// if condition
+
+// var a = 10;
+// var b = 20;
+
+// if(a > b){
+//     console.log('a is greater then b');
+// }
+// if(a < b){
+//     console.log('a is less then b');
+// }
+
+// var n = 50;
+
+// // check n is even or odd
+// if(n % 2 == 0){
+//     console.log(n+' is even number');
+// }
+
+// if(n % 2 == 1){
+//     console.log(n+' is Odd number');
+// }
+
+// if-else condition 
+
 var a = 10;
 var b = 20;
+ if(a > b){
+     console.log('a is greater then b');
+ }else{
+     console.log('a is less then b');
+ }
 
-if(a > b){
-    console.log('a is greater then b');
-}
-if(a < b){
-    console.log('a is less then b');
-}
+ var n = 50;
+ // check odd or even
+ if(n % 2 === 0){
+     console.log(n+ ' is a even number');
+ }else{
+     console.log(n+ ' is a odd number');
+ }
 
-var n = 50;
-
-// check n is even or odd
-if(n % 2 == 0){
-    console.log(n+' is even number');
-}
-
-if(n % 2 == 1){
-    console.log(n+' is Odd number');
-}
