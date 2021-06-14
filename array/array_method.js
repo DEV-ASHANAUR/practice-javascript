0e2c164f72191d0bdf1225586c437a0d4c4494f3
dev-ashanaur <devashanaur009@gmail.com.com>
2021-06-15 01:47:10 +0600

    learn some array method

---
+++ array/array_method.js
@@ -0,0 +1,14 @@
+var arr = [1,2,3,4,5,6,7];
+
+// var arr2 = [5,68,7,4];
+
+// var arr3 = arr.concat(arr2) //concate array
+
+//console.log(arr.join(', ')); // array to string
+
+//console.log(Array.isArray(arr)) // check array or not
+
+var arr4 = Array.from(arr); // clone array
+
+console.log(arr4);
+
