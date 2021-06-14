a58b15e0da1de88bc1e02937036fb3b097fb0a43
dev-ashanaur <devashanaur009@gmail.com.com>
2021-06-14 23:16:36 +0600

    learn about array declaration

---
+++ array/array_declaration.js
@@ -0,0 +1,7 @@
+// simple way 
+
+var arr = [1,2,3,4];
+arr[4] = 5; // insert value
+arr[2] = 8; // update or replace value
+console.log(arr);//print array
+console.log(arr.length);//print array length
