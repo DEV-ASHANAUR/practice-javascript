dfb62b6fe09e441ce27a549a70beab3517fa7cf0
dev-ashanaur <devashanaur009@gmail.com.com>
2021-06-15 01:00:16 +0600

    learn about array_operation

---
+++ array/array_operation.js
@@ -0,0 +1,13 @@
+var arr = [1,2,3,4,5,6,7,8];
+//arr[5] = 9; // manual system for insert a value
+//arr.push(9); // insert value at the end
+//arr.unshift(9); // insert value at the first
+//arr.splice(3,0,9,12); // (index,delete,...insert)
+
+//arr.pop(); //delete value at the end
+//arr.shift(); //delete value at the first
+//arr.splice(1,2); //(index,delete)
+
+// arr.splice(2,1,4) // update
+
+console.log(arr);
