df70552ae2ed9139f6d9672ecaf8adce11af01e0
dev-ashanaur <devashanaur009@gmail.com.com>
2021-06-15 12:23:55 +0600

    learn accessing object propertise

---
+++ object/object_accessing.js
@@ -0,0 +1,18 @@
+// object accessing
+
+var obj = {
+    x:10,
+    y:20,
+    z:30
+}
+
+// object access by dot notation
+
+// console.log(obj.x); // output is 10
+// console.log(obj.x + obj.y) // output is 30
+
+// object access by array notation
+
+console.log(obj['x']) // output is 10
+console.log(obj['x'] + obj['y']); //output is 30
+
