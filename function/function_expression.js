552c08763dd4898b14fc2bf3ec550306c18d420d
dev-ashanaur <devashanaur009@gmail.com.com>
2021-06-16 00:44:23 +0600

    learn function expression

---
+++ function/function_expression.js
@@ -0,0 +1,13 @@
+var addition = function(a,b){
+    return a+b;
+}
+
+// console.log(addition(4,5));
+
+// var add = addition;
+
+// console.log(add(32,34));
+
+setTimeout(function(){
+    console.log('i will come within five secound');
+},5000);
