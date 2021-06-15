0eda38cf782c98318e59de00759998c01c5189dd
dev-ashanaur <devashanaur009@gmail.com.com>
2021-06-15 12:05:07 +0600

    learn about obj declaration

---
+++ object/object_declaration.js
@@ -0,0 +1,22 @@
+//object declar by litaral
+
+// var obj = {};
+
+// console.log(typeof obj);
+
+// var obj = {
+//     x:10,
+//     y:20
+// }
+// obj.z = 30; // assign value into z key
+// console.log(obj);
+
+// object construct
+
+// var obj = Object();
+
+var obj = new Object(); // another system of oop style object declaration
+
+obj.a = 10 // assign a value
+
+console.log(obj);
