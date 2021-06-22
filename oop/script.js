fb410c038b5bbd0a3255a83b3d5b0fbfea614dfb
dev-ashanaur <devashanaur009@gmail.com.com>
2021-06-22 20:27:36 +0600

    learn about descriptor

--- oop/script.js
+++ oop/script.js
@@ -46,22 +46,55 @@
 
 // Constructor pattern
 
-var Rectangle = function(width,height){
-    this.width = width
-    this.height = height
+// var Rectangle = function(width,height){
+//     this.width = width
+//     this.height = height
 
-    this.draw = function(){
-        console.log("I am a Rectangle")
-        this.printPropertise()
-        console.log(this)
-    }
+//     this.draw = function(){
+//         console.log("I am a Rectangle")
+//         this.printPropertise()
+//         console.log(this)
+//     }
 
-    this.printPropertise = function(){
-        console.log('width is :'+this.width)
-        console.log('height is :'+this.height)
-    }
+//     this.printPropertise = function(){
+//         console.log('width is :'+this.width)
+//         console.log('height is :'+this.height)
+//     }
+// }
+
+// var rect3 = new Rectangle(400,200)
+// rect3.draw();
+
+// learn about descriptor
+
+// var person = {
+//     name: 'Ashanur'
+// }
+
+// console.log(person)
+
+// for(var i in person){
+//     console.log(i);
+// }
+
+// var descriptor = Object.getOwnPropertyDescriptor(person,'name');
+
+// console.log(descriptor);
+
+// let baseObj = Object.getPrototypeOf(person);
+
+// let descriptor = Object.getOwnPropertyDescriptor(baseObj,'toString')
+
+// console.log(descriptor)
+
+var person = {
+    name: 'Ashanur'
 }
 
-var rect3 = new Rectangle(400,200)
-rect3.draw();
-
+Object.defineProperty(person,"name",{
+    enumerable:false, // loop able or not
+    writable:false, // overwrite able or not
+    configurable:false, // remove able or not
+    value: "Md Ashanur Rahman" // default value set
+});
+// end learn about descriptor
